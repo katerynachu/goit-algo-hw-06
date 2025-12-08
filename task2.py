aad10bc36afb5c545f56b1794cdc3c4ff548400f
Kateryna <k.churuta@gmail.com>
2025-12-08 17:19:30 +0000

    update fn handcrafted

--- task2.py
+++ task2.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from collections import deque
 
 G = nx.Graph()
 
@@ -17,9 +18,45 @@ G.add_edges_from([
 start_node = "Work Laptop"
 end_node = "Tablet"
 
-path_bfs = nx.shortest_path(G,start_node,end_node)
 
-path_dfs = list(nx.all_simple_paths(G,start_node,end_node))[0]
+def bfs_find_path(graph,start,end):
+    queue = deque([[start]])
+    visited = set()
+
+    while queue:
+        path = queue.popleft()  
+        node = path[-1] 
+
+        if node == end:
+            return path
+
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph.neighbors(node):
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+    return None            
+    
+def dfs_find_path(graph, start, end,path=None):
+    if path is None:
+        path = []
+    path.append(start)
+    if start == end:
+        return path
+    for neighbor in graph.neighbors(start):
+        if neighbor not in path:
+            result = dfs_find_path(graph, neighbor, end, path)
+            if result:
+                return result
+    path.pop()  
+                
+    return None 
+    
+path_bfs = bfs_find_path(G,start_node,end_node)
+
+
+path_dfs = dfs_find_path(G,start_node,end_node)
 
 print(f"Шлях BFS (ширина): {path_bfs}")
 print(f"Шлях DFS (глибина): {path_dfs}")
